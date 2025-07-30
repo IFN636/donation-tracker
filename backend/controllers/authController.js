2dbe2a1bf661b13cffc010c03dcdcc1e7e693527
Khang Vo <volenhatkhang1808@gmail.com>
2025-07-30 16:12:44 +1000

    rename project

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,20 +1,25 @@
-
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
 const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         const userExists = await User.findOne({ email });
-        if (userExists) return res.status(400).json({ message: 'User already exists' });
+        if (userExists)
+            return res.status(400).json({ message: "User already exists" });
 
         const user = await User.create({ name, email, password });
-        res.status(201).json({ id: user.id, name: user.name, email: user.email, token: generateToken(user.id) });
+        res.status(201).json({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user.id),
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -25,9 +30,14 @@ const loginUser = async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (user && (await bcrypt.compare(password, user.password))) {
-            res.json({ id: user.id, name: user.name, email: user.email, token: generateToken(user.id) });
+            res.json({
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user.id),
+            });
         } else {
-            res.status(401).json({ message: 'Invalid email or password' });
+            res.status(401).json({ message: "Invalid email or password" });
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -36,26 +46,26 @@ const loginUser = async (req, res) => {
 
 const getProfile = async (req, res) => {
     try {
-      const user = await User.findById(req.user.id);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      res.status(200).json({
-        name: user.name,
-        email: user.email,
-        university: user.university,
-        address: user.address,
-      });
+        const user = await User.findById(req.user.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            name: user.name,
+            email: user.email,
+            university: user.university,
+            address: user.address,
+        });
     } catch (error) {
-      res.status(500).json({ message: 'Server error', error: error.message });
+        res.status(500).json({ message: "Server error", error: error.message });
     }
-  };
+};
 
 const updateUserProfile = async (req, res) => {
     try {
         const user = await User.findById(req.user.id);
-        if (!user) return res.status(404).json({ message: 'User not found' });
+        if (!user) return res.status(404).json({ message: "User not found" });
 
         const { name, email, university, address } = req.body;
         user.name = name || user.name;
@@ -64,7 +74,14 @@ const updateUserProfile = async (req, res) => {
         user.address = address || user.address;
 
         const updatedUser = await user.save();
-        res.json({ id: updatedUser.id, name: updatedUser.name, email: updatedUser.email, university: updatedUser.university, address: updatedUser.address, token: generateToken(updatedUser.id) });
+        res.json({
+            id: updatedUser.id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            university: updatedUser.university,
+            address: updatedUser.address,
+            token: generateToken(updatedUser.id),
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
