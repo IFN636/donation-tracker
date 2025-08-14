0073840188b7b1cac298d241d581b8b03867bb2f
Khang Vo <volenhatkhang1808@gmail.com>
2025-08-15 02:24:25 +1000

    Test env React app

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const baseURL =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:5001"
-        : "http://3.24.15.154:5001";
+// const baseURL =
+//     process.env.NODE_ENV === "development"
+//         ? "http://localhost:5001"
+//         : "http://3.24.15.154:5001";
+
 const axiosInstance = axios.create({
-    baseURL: baseURL,
+    baseURL: "http://3.24.15.154:5001",
     headers: { "Content-Type": "application/json" },
 });
 
