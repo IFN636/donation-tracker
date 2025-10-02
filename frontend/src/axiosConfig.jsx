1652fa0abcfcea239b5eaf4130fae83d172263af
Khang Vo <volenhatkhang1808@gmail.com>
2025-10-03 02:15:32 +1000

    update api url axios

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const BASE_URL = isProduction
-    ? "https://ifn636-donation.khangvo.dev"
-    : "http://localhost:5001";
-
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: "https://ifn636-donation.khangvo.dev",
+    // baseURL: "http://localhost:5001",
     headers: { "Content-Type": "application/json" },
 });
 
