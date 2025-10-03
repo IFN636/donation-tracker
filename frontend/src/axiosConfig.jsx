547bbd7a0318cf71d4dbbdc26ac4340b3b5d0f71
Khang Vo <volenhatkhang1808@gmail.com>
2025-10-03 20:26:21 +1000

    update url

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: "https://ifn636-donation.khangvo.dev",
+    baseURL: "https://ifn636-donation.khangvo.dev",
     // baseURL: "http://localhost:5001",
     headers: { "Content-Type": "application/json" },
 });
